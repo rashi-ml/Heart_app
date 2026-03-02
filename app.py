7660cbaa26b59fe235e492af51163248c2fcfc04
Rashi jain <rashi17032006@gmail.com>
2026-03-02 12:27:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd 
+import joblib
+
+model=joblib.load('KNN_heart.pkl')
+scaler=joblib.load('scaler.pkl')
+columns=joblib.load('columns.pkl')
+
+
+st.title('Heart Disease Prediction')
+st.markdown("Provide the following details to check your heart stroke risk:")
+#input data
+age = st.slider("Age", 18, 100, 40)
+sex = st.selectbox("Sex", ["M", "F"])
+chest_pain = st.selectbox("Chest Pain Type", ["ATA", "NAP", "TA", "ASY"])
+resting_bp = st.number_input("Resting Blood Pressure (mm Hg)", 80, 200, 120)
+cholesterol = st.number_input("Cholesterol (mg/dL)", 100, 600, 200)
+fasting_bs = st.selectbox("Fasting Blood Sugar > 120 mg/dL", [0, 1])
+resting_ecg = st.selectbox("Resting ECG", ["Normal", "ST", "LVH"])
+max_hr = st.slider("Max Heart Rate", 60, 220, 150)
+exercise_angina = st.selectbox("Exercise-Induced Angina", ["Y", "N"])
+oldpeak = st.slider("Oldpeak (ST Depression)", 0.0, 6.0, 1.0)
+st_slope = st.selectbox("ST Slope", ["Up", "Flat", "Down"])
+
+# when predict is clicked
+if st.button("Predict"):
+    #create a raw dictionary 
+    input_data={
+        'age': age, 
+        'RestingBP': resting_bp,
+        'Cholesterol': cholesterol,
+        'FastingBS': fasting_bs,
+        'MaxHR': max_hr,
+        'Oldpeak': oldpeak,
+        'Sex_' + sex: 1,
+        'ChestPainType_' + chest_pain: 1,
+        'RestingECG_' + resting_ecg: 1,
+        'ExerciseAngina_' + exercise_angina: 1,
+        'ST_Slope_' + st_slope: 1
+
+
+    }
+    # create a dataframe with all columns
+    input_df=pd.DataFrame([input_data])
+
+    # fill missing columns with 0
+    for col in columns:
+        if col not in input_df.columns:
+            input_df[col]=0
+    # Reorder columns
+    input_df=input_df[columns]
+    # scale the data    
+    input_scaled=scaler.transform(input_df)
+
+    # Make prediction
+    prediction=model.predict(input_scaled)[0]
+    # Display result
+    if prediction==1:
+        st.error("High risk of heart disease. Please consult a doctor.")
+    else:
+        st.success("Low risk of heart disease. Keep up the healthy lifestyle!")
+
